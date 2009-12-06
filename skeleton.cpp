12badbe0f9d52e7a652bc2d864266f7dd9c8befc
jmimu <jmimu@free.fr>
2009-12-06 19:10:52 +0100

    minor uppdates

--- skeleton.cpp
+++ skeleton.cpp
@@ -18,14 +18,14 @@ void Skeleton::make_object()
   Skel_Edge *r_arm2=new Skel_Edge(r_arm1,40,-30);
 
   Skel_Edge *body=new Skel_Edge(&origin,50,90);
-  Skel_Edge *l_leg1=new Skel_Edge(body,40,45);
-  l_leg1->add_image("leg2.png");
+  Skel_Edge *l_leg1=new Skel_Edge(body,40,120);
+  l_leg1->add_image("leg.png",true);
   Skel_Edge *l_leg2=new Skel_Edge(l_leg1,50,90);
-  //l_leg2->add_image("l_foot.png");
+  l_leg2->add_image("r_foot.png", true);
   Skel_Edge *r_leg1=new Skel_Edge(body,40,60);
-  r_leg1->add_image("leg2.png",true);
+  r_leg1->add_image("leg.png");
   Skel_Edge *r_leg2=new Skel_Edge(r_leg1,50,90);
-  //r_leg2->add_image("r_foot.png");
+  r_leg2->add_image("r_foot.png");
 
 }
 
